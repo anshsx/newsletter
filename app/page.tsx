a3eb52254b85b7a2faa5347b9edc21ed06b70106
anshsx <erasearch.co@gmail.com>
2024-10-25 15:49:03 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,8 @@
 
 import { useState } from 'react';
 import { Menu, X } from 'lucide-react';
-import Image from 'next/image'
 import Link from 'next/link'
 import era from '../era.png'
-import company from '../company.png'
 
 // Import CSS for fonts and global styles
 import '../app/globals.css';

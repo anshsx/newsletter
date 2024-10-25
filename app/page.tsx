86a8b109b9955e1750f5902ce83c3f7121b2c1a5
anshsx <erasearch.co@gmail.com>
2024-10-25 15:41:46 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,10 @@
 import { useState } from 'react';
 import { Menu, X } from 'lucide-react';
 import Link from 'next/link';
+import Image from 'next/image'
+import Link from 'next/link'
+import era from '../era.png'
+import company from '../company.png'
 
 // Import CSS for fonts and global styles
 import '../app/globals.css';
@@ -223,7 +227,7 @@ export default function Component() {
         <footer className="flex flex-col items-center pb-12">
           <button className="flex items-center justify-center space-x-2 px-4 py-2.5 bg-[#404450] text-[#C9CDD9] rounded-lg border border-gray-300">
             <img
-              src="/placeholder.svg?height=50&width=200"
+              src="{era}?height=50&width=200"
               alt="Custom footer image"
               className="w-10 h-10"
             />
